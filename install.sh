62b41bf0bc1ffe770d7bab60712156cb30479c86
fensoft <fensoft@gmail.com>
2023-02-03 02:25:08 +0100

    add speed

--- install.sh
+++ install.sh
@@ -8,11 +8,22 @@ while sleep 5; do
     SPEED=\`ethtool \$i | grep "Speed: " | sed "s#.*: ##" | sed "s#Unknown!#No Speed#" | sed "s#b/s##"\`
     RX=\`ifconfig \$i | grep "RX bytes:" | sed "s#.*RX bytes:\([0-9]*\) .*#\\1#"\`
     TX=\`ifconfig \$i | grep "TX bytes:" | sed "s#.*TX bytes:\([0-9]*\) .*#\\1#"\`
-
+    eval "OLDRX=\\$RX_\$i"
+    if [ "\$OLDRX" ]; then
+      eval "RXSPD=\\$(((\$RX-\$OLDRX)/\$SLEEP))"
+    fi
+    eval "RX_\$i=\$RX"
+    eval "OLDTX=\\$TX_\$i"
+    if [ "\$OLDTX" ]; then
+      eval "TXSPD=\\$(((\$TX-\$OLDTX)/\$SLEEP))"
+    fi
+    eval "TX_\$i=\$TX"
     mosquitto_pub -h \$2 -t openwrt/$HOSTNAME/\$i/link -m "\$LINK"
     mosquitto_pub -h \$2 -t openwrt/$HOSTNAME/\$i/speed -m "\$SPEED"
     mosquitto_pub -h \$2 -t openwrt/$HOSTNAME/\$i/rx -m "\$RX"
     mosquitto_pub -h \$2 -t openwrt/$HOSTNAME/\$i/tx -m "\$TX"
+    mosquitto_pub -h \$2 -t openwrt/$HOSTNAME/\$i/rxspd -m "\$RXSPD"
+    mosquitto_pub -h \$2 -t openwrt/$HOSTNAME/\$i/txspd -m "\$TXSPD"
   done
 done
 EOF
